eb335a5972e805a5b39ce1443187bf336eff5cbb
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-01-13 23:15:42 -0300

    Adds CardController

---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    [SerializeField]
+    private Card card;
+
+    public Card Card
+    {
+        get
+        {
+            return card;
+        }
+        set
+        {
+            if (card == null)
+            {
+                this.card = value;
+            }
+        }
+    }
+}
